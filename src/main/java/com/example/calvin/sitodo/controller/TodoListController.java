a5cd25986a17c23a4ec5f9c61eaddf1d5f147902
Calvin Hartanto <calvinhonggare@gmail.com>
2022-09-28 14:56:00 +0700

    test add item

--- src/main/java/com/example/calvin/sitodo/controller/TodoListController.java
+++ src/main/java/com/example/calvin/sitodo/controller/TodoListController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.calvin.sitodo.model.TodoItem;
 import com.example.calvin.sitodo.service.TodoListService;
@@ -23,6 +25,16 @@ public class TodoListController {
     @GetMapping("/list")
     public String showList(Model model) {
         List<TodoItem> todoItems = todoListService.getTodoItems();
+
+        model.addAttribute("todoList", todoItems);
+
+        return "list";
+    }
+    @PostMapping("/list")
+    public String addItemToList(@ModelAttribute TodoItem todoItem, Model model) {
+    	todoListService.addTodoItem(todoItem);
+    	
+    	List<TodoItem> todoItems = todoListService.getTodoItems();
 
         model.addAttribute("todoList", todoItems);
 
